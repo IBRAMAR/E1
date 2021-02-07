949e4b7ad98a2609cfaeba0b723ac1dfbf841400
Your Name <you@example.com>
2021-02-07 19:50:10 +0100

    nettoyage

--- project.sql
+++
@@ -1,6 +0,0 @@
---PRAGMA table_info(doctor);
---PRAGMA foreign_key_list(doctor);
---SELECT * FROM data LEFT JOIN doctor ON data.doctor=doctor.doctor where doctor.institution = 'labo 1' ;
---PRAGMA table_info(doctor);
-
-SELECT COUNT(compagny) FROM ds WHERE compagny='Compagny C';
